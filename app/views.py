c678152612bbd1701fb1ca4afc3b2764583deba8
Shohruxshoh <shohhhhhhhhhhhhhhhhhhhh@gmail.com>
2025-03-11 02:40:43 +0500

    first commit

--- app/views.py
+++ app/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from .forms import MyModelForm
+from .models import MyModel
 # Create your views here.
 
 def home(request):
+    lists = MyModel.objects.all()
     form = MyModelForm(request.POST, request.FILES)
     if request.method == "POST":
         if form.is_valid():
             form.save()
-    return render(request, 'home.html', {"form": form})
+    return render(request, 'home.html', {"form": form, "objects":lists})
